15fa9b951ed58a2c547651b278950ebac30f6680
Alan Amador <alanamadors96@gmail.com>
2026-05-28 12:32:13 -0600

    chore: update authConfig

--- iave-web/src/auth/authConfig.js
+++ iave-web/src/auth/authConfig.js
@@ -1,7 +1,7 @@
 import { UserManager, WebStorageStateStore } from "oidc-client-ts";
 
 // --- CONFIGURACIÓN DEL SERVIDOR DE IDENTIDAD ---
-const AUTHORITY_URL = "https://192.168.1.250:8086";
+const AUTHORITY_URL = "https://devatm.auth.atmexicana.com.mx";
 const CLIENT_ID = "DevIAVE_ATM";
 // NOTA: Para Resource Owner Password, a veces se requiere Client Secret si el cliente no es "public".
 // Si te da error "invalid_client", se debe configurar el secreto o configurar el cliente como público.
@@ -9,11 +9,11 @@ const CLIENT_SECRET = "";
 
 const METADATA = {
     issuer: AUTHORITY_URL,
-    authorization_endpoint: "https://192.168.1.250:8086/oauth2/authorize", 
-    token_endpoint: "https://192.168.1.250:8086/oauth2/token", // <--- Endpoint correcto
-    userinfo_endpoint: "https://192.168.1.250:8086/userinfo", // <--- Endpoint correcto
-    end_session_endpoint: "https://192.168.1.250:8086/connect/logout", // <--- Endpoint correcto
-    jwks_uri: "https://192.168.1.250:8086/oauth2/jwks",
+    authorization_endpoint: "https://devatm.auth.atmexicana.com.mx/oauth2/authorize", 
+    token_endpoint: "https://devatm.auth.atmexicana.com.mx/oauth2/token", // <--- Endpoint correcto
+    userinfo_endpoint: "https://devatm.auth.atmexicana.com.mx/userinfo", // <--- Endpoint correcto
+    end_session_endpoint: "https://devatm.auth.atmexicana.com.mx/connect/logout", // <--- Endpoint correcto
+    jwks_uri: "https://devatm.auth.atmexicana.com.mx/oauth2/jwks",
 };
 
 const oidcConfig = {
